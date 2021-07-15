477e055a082cb8dcdcfe1cee5662d3de943fe716
JaeYoonMyung <77507968+jaeyoon492@users.noreply.github.com>
2021-07-15 21:26:11 +0900

    메소드, 변수 이름 리펙터링

--- src/LastNameCollector.java
+++ src/LastNameCollector.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class LastNameCollector {
     Scanner scanner = new Scanner(System.in);
     private String lastName;
-    private final int USER_INPUT_NUM_OF_NAME = 5;
+    private final int criteria = 5;
     private int lastNameCount;
-    private final ArrayList<String> USER_PICK_LAST_NAME_ARRAY_LIST = new ArrayList<>();
-    private final ArrayList<String> NOT_USER_PICK_LAST_NAME_ARRAY_LIST = new ArrayList<>();
-    
+    private final ArrayList<String> userPickedList = new ArrayList<>();
+    private final ArrayList<String> notUserPickList = new ArrayList<>();
+
     public void run() {
         displayInputMessage();
         ArrayList<String> nameList = getName();
-        getLastName();
+        lastName = getLastName();
         filter(nameList, lastName);
         displayOutputMessage();
     }
 
-    private void getLastName() {
+    private String getLastName() {
         System.out.println("=================================");
         System.out.println("어떤 성씨를 골라줄까요?");
         System.out.println("=================================");
@@ -28,34 +28,35 @@ public class LastNameCollector {
             System.out.println("=================================");
             getLastName();
         } else {
-            this.lastName = userInput;
+            lastName = userInput;
         }
+        return lastName;
     }
 
     private void filter(ArrayList<String> nameList, String delimiter) {
-        for (int index = 0; index < USER_INPUT_NUM_OF_NAME; index++) {
-            setUserPickLastNameList(nameList, delimiter, lastNameCount, USER_PICK_LAST_NAME_ARRAY_LIST, index);
-            setNotUserPickLastNameList(nameList, delimiter, NOT_USER_PICK_LAST_NAME_ARRAY_LIST, index);
+        for (int index = 0; index < criteria; index++) {
+            setUserPick(nameList, delimiter, lastNameCount, userPickedList, index);
+            setNotUserPick(nameList, delimiter, notUserPickList, index);
         }
-        if (NOT_USER_PICK_LAST_NAME_ARRAY_LIST.size() == USER_INPUT_NUM_OF_NAME) {
-            displayNotExistUserPickLastName();
+        if (notUserPickList.size() == criteria) {
+            displayNoBody();
         }
     }
 
-    private void displayNotExistUserPickLastName() {
+    private void displayNoBody() {
         System.out.println("=================================");
         System.out.println(lastName + "씨가 한명도 존재하지 않습니다.ㅠㅅㅠ");
         System.out.println("=================================");
     }
 
-    private void setUserPickLastNameList(ArrayList<String> nameList, String delimiter, int lastNameCount, ArrayList<String> userPickArrayList, int index) {
+    private void setUserPick(ArrayList<String> nameList, String delimiter, int lastNameCount, ArrayList<String> userPickArrayList, int index) {
         if (nameList.get(index).substring(0, 1).equals(delimiter)) {
             userPickArrayList.add(nameList.get(index));
             this.lastNameCount = ++lastNameCount;
         }
     }
 
-    private void setNotUserPickLastNameList(ArrayList<String> nameList, String delimiter, ArrayList<String> notUserPickArrayList, int index) {
+    private void setNotUserPick(ArrayList<String> nameList, String delimiter, ArrayList<String> notUserPickArrayList, int index) {
         if (!nameList.get(index).substring(0, 1).equals(delimiter)) {
             notUserPickArrayList.add(nameList.get(index));
         }
@@ -63,7 +64,7 @@ public class LastNameCollector {
 
     private ArrayList<String> getName() {
         ArrayList<String> nameList = new ArrayList<>();
-        for (int index = 0; index < USER_INPUT_NUM_OF_NAME; index++) {
+        for (int index = 0; index < criteria; index++) {
             nameList.add(scanner.next());
         }
         return nameList;
@@ -78,21 +79,22 @@ public class LastNameCollector {
 
     private void displayOutputMessage() {
         if (lastNameCount > 0) {
-            displayNumberOfUserPickLastName();
+            displayCount(lastName ,lastNameCount, userPickedList);
         }
-        if (NOT_USER_PICK_LAST_NAME_ARRAY_LIST.size() > 0) {
-            displayNotUserPickLastNameList();
+        if (notUserPickList.size() > 0) {
+            displayNotMatching(notUserPickList, lastName);
         }
     }
 
-    private void displayNotUserPickLastNameList() {
-        System.out.println(NOT_USER_PICK_LAST_NAME_ARRAY_LIST + "님은 " + lastName + "씨가 아닙니다.");
+    private void displayNotMatching(ArrayList<String> notUserPickList, String lastName) {
+        System.out.println(notUserPickList + "님은 " + lastName + "씨가 아닙니다.");
         System.out.println("=================================");
     }
 
-    private void displayNumberOfUserPickLastName() {
+    private void displayCount(String lastName , int lastNameCount, ArrayList<String> userPickedList ) {
         System.out.println("=================================");
-        System.out.println(lastName + "씨는 총 " + lastNameCount + "명이며 " + USER_PICK_LAST_NAME_ARRAY_LIST + "님 입니다.");
+        System.out.println(lastName + "씨는 총 " + lastNameCount + "명이며 " + userPickedList + "님 입니다.");
         System.out.println("=================================");
     }
 }
+
